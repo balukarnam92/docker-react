caf5a638d9cbdfa6b541764a4b9c6bc3dfb148a8
balukarnam <karnambalasubramanyam@gmail.com>
2023-05-15 16:52:12 +0530

    new

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi, Suresh, Balu, Sai, Kalpana, Mamatha, Pooja, Sumana -  how are you</p>
+        <p>Hi Suresh, Balu, Sai, Kalpana, Mamatha, Pooja, Sumana -  how are you</p>
         <a
           className="App-link"
           href="https://reactjs.org"
